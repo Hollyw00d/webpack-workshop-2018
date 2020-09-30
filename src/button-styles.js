09fabe83f8736d249c432f00eec87f667301ff2e
Matt Jennings <matt@mattjennings.net>
2020-09-30 16:49:01 -0700

    added more ESM modules and bundled them using webpack

---
+++ src/button-styles.js
@@ -0,0 +1,8 @@
+const red = 'red';
+const blue = 'blue';
+const makeColorStyle = (color) => `color: ${color};`;
+
+// exports.red = red;
+// exports.blue = blue;
+// exports.makeColorStyle = makeColorStyle;
+export { red, blue, makeColorStyle };
