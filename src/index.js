9bf18a9235c64e8c1db5caef9018cdbd1bed3ecf
Matt Jennings <matt@mattjennings.net>
2020-10-01 18:28:29 -0700

    added footer tag with content for webpack server example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import nav from "./nav";
-import { top, bottom } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import { footer } from './footer';
 
 const button = makeButton('Yay a button!!!!');
+button.style = makeColorStyle('purple');
 document.body.appendChild(button);
+document.body.appendChild(footer);
